e0606c1924bfd33913f140f0be79412278637ec1
noahellis <noah_ellis@hotmail.com>
2016-03-04 08:17:38 -0600

    initial java push

---
+++ Java Project 1/src/SalaryComputation1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class SalaryComputation1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Enter your wage");
+		double wage= Double.parseDouble(scanner.next());
+		System.out.println("Enter your hours:");
+		double hours=Double.parseDouble(scanner.next());
+		
+		double netPay=wage*hours;
+		
+		NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.US);
+		
+		String payFormatted = formatter.format(netPay);
+		
+			System.out.println("you are awarded" + payFormatted);
+			
+		
+		
+		
+		
+	}
+
+}
